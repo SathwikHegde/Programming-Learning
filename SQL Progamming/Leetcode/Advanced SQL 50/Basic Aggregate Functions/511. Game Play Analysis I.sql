f767ca02e6b711f7bbe44ee599db5dd9702fb58b
SathwikHegde <hegde.sat@northeastern.edu>
2025-01-28 23:23:21 -0700

    Leetcode SQL 28th Jan 25

---
+++ SQL Progamming/Leetcode/Advanced SQL 50/Basic Aggregate Functions/511. Game Play Analysis I.sql
@@ -0,0 +1,29 @@
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+
+
+SQL Schema:
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int)
+Truncate table Activity
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5')
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-05-02', '6')
+insert into Activity (player_id, device_id, event_date, games_played) values ('2', '3', '2017-06-25', '1')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5')
+
+# Write your MySQL query statement below
+
+SELECT player_id, MIN(event_date) AS first_login  
+FROM Activity
+GROUP BY player_id;
