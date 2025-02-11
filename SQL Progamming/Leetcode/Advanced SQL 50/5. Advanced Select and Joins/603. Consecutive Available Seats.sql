b8815ba2e8e27ef4e796611696c41794246c7f20
SathwikHegde <hegde.sat@northeastern.edu>
2025-02-10 21:13:08 -0700

    Leetcode SQL 10th Feb 25

---
+++ SQL Progamming/Leetcode/Advanced SQL 50/5. Advanced Select and Joins/603. Consecutive Available Seats.sql
@@ -0,0 +1,62 @@
+Table: Cinema
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| seat_id     | int  |
+| free        | bool |
++-------------+------+
+seat_id is an auto-increment column for this table.
+Each row of this table indicates whether the ith seat is free or not. 1 means free while 0 means occupied.
+ 
+
+Find all the consecutive available seats in the cinema.
+
+Return the result table ordered by seat_id in ascending order.
+
+The test cases are generated so that more than two seats are consecutively available.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Cinema table:
++---------+------+
+| seat_id | free |
++---------+------+
+| 1       | 1    |
+| 2       | 0    |
+| 3       | 1    |
+| 4       | 1    |
+| 5       | 1    |
++---------+------+
+Output: 
++---------+
+| seat_id |
++---------+
+| 3       |
+| 4       |
+| 5       |
++---------+
+
+SQL Schema:-
+Create table If Not Exists Cinema (seat_id int primary key auto_increment, free bool)
+Truncate table Cinema
+insert into Cinema (seat_id, free) values ('1', '1')
+insert into Cinema (seat_id, free) values ('2', '0')
+insert into Cinema (seat_id, free) values ('3', '1')
+insert into Cinema (seat_id, free) values ('4', '1')
+insert into Cinema (seat_id, free) values ('5', '1')
+
+# Write your MySQL query statement below
+
+
+SELECT DISTINCT t1.seat_id 
+FROM cinema AS t1 
+JOIN cinema AS t2 
+ON abs(t1.seat_id - t2.seat_id) = 1 
+AND t1.free =1 AND t2.free =1 
+ORDER BY t1.seat_id ; 
