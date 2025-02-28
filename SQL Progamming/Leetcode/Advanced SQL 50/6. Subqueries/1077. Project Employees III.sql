2b333835b989923c141fcbe95c3517102aad3b92
SathwikHegde <hegde.sat@northeastern.edu>
2025-02-27 20:05:55 -0700

    Leetcode SQL 27th Feb 25

---
+++ SQL Progamming/Leetcode/Advanced SQL 50/6. Subqueries/1077. Project Employees III.sql
@@ -0,0 +1,92 @@
+Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key (combination of columns with unique values) of this table.
+employee_id is a foreign key (reference column) to Employee table.
+Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+ 
+
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key (column with unique values) of this table.
+Each row of this table contains information about one employee.
+ 
+
+Write a solution to report the most experienced employees in each project. In case of a tie, report all employees with the maximum number of experience years.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 3                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+Output: 
++-------------+---------------+
+| project_id  | employee_id   |
++-------------+---------------+
+| 1           | 1             |
+| 1           | 3             |
+| 2           | 1             |
++-------------+---------------+
+Explanation: Both employees with id 1 and 3 have the most experience among the employees of the first project. For the second project, the employee with id 1 has the most experience.
+
+SQL Schema:-
+Create table If Not Exists Project (project_id int, employee_id int)
+Create table If Not Exists Employee (employee_id int, name varchar(10), experience_years int)
+Truncate table Project
+insert into Project (project_id, employee_id) values ('1', '1')
+insert into Project (project_id, employee_id) values ('1', '2')
+insert into Project (project_id, employee_id) values ('1', '3')
+insert into Project (project_id, employee_id) values ('2', '1')
+insert into Project (project_id, employee_id) values ('2', '4')
+Truncate table Employee
+insert into Employee (employee_id, name, experience_years) values ('1', 'Khaled', '3')
+insert into Employee (employee_id, name, experience_years) values ('2', 'Ali', '2')
+insert into Employee (employee_id, name, experience_years) values ('3', 'John', '3')
+insert into Employee (employee_id, name, experience_years) values ('4', 'Doe', '2')a
+
+# Write your MySQL query statement below
+
+SELECT 
+    project_id, employee_id
+FROM (
+SELECT p.project_id, p.employee_id, e.experience_years, max(e.experience_years) OVER (PARTITION BY project_id ORDER BY e.experience_years DESC) mx
+FROM Project p
+INNER JOIN Employee e
+ON p.employee_id = e.employee_id 
+) t
+WHERE mx = experience_years
