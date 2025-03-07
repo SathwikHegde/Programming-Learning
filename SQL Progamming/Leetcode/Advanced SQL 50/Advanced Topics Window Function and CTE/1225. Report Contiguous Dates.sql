8be764148638f33580fa03bf0d87710017d0c90a
SathwikHegde <hegde.sat@northeastern.edu>
2025-03-06 18:18:49 -0700

    Leetcode SQL 6th Mar 25

---
+++ SQL Progamming/Leetcode/Advanced SQL 50/Advanced Topics Window Function and CTE/1225. Report Contiguous Dates.sql
@@ -0,0 +1,117 @@
+Table: Failed
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| fail_date    | date    |
++--------------+---------+
+fail_date is the primary key (column with unique values) for this table.
+This table contains the days of failed tasks.
+ 
+
+Table: Succeeded
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| success_date | date    |
++--------------+---------+
+success_date is the primary key (column with unique values) for this table.
+This table contains the days of succeeded tasks.
+ 
+
+A system is running one task every day. Every task is independent of the previous tasks. The tasks can fail or succeed.
+
+Write a solution to report the period_state for each continuous interval of days in the period from 2019-01-01 to 2019-12-31.
+
+period_state is 'failed' if tasks in this interval failed or 'succeeded' if tasks in this interval succeeded. Interval of days are retrieved as start_date and end_date.
+
+Return the result table ordered by start_date.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Failed table:
++-------------------+
+| fail_date         |
++-------------------+
+| 2018-12-28        |
+| 2018-12-29        |
+| 2019-01-04        |
+| 2019-01-05        |
++-------------------+
+Succeeded table:
++-------------------+
+| success_date      |
++-------------------+
+| 2018-12-30        |
+| 2018-12-31        |
+| 2019-01-01        |
+| 2019-01-02        |
+| 2019-01-03        |
+| 2019-01-06        |
++-------------------+
+Output: 
++--------------+--------------+--------------+
+| period_state | start_date   | end_date     |
++--------------+--------------+--------------+
+| succeeded    | 2019-01-01   | 2019-01-03   |
+| failed       | 2019-01-04   | 2019-01-05   |
+| succeeded    | 2019-01-06   | 2019-01-06   |
++--------------+--------------+--------------+
+Explanation: 
+The report ignored the system state in 2018 as we care about the system in the period 2019-01-01 to 2019-12-31.
+From 2019-01-01 to 2019-01-03 all tasks succeeded and the system state was "succeeded".
+From 2019-01-04 to 2019-01-05 all tasks failed and the system state was "failed".
+From 2019-01-06 to 2019-01-06 all tasks succeeded and the system state was "succeeded".
+
+SQL Schema:-
+
+Create table If Not Exists Failed (fail_date date)
+Create table If Not Exists Succeeded (success_date date)
+Truncate table Failed
+insert into Failed (fail_date) values ('2018-12-28')
+insert into Failed (fail_date) values ('2018-12-29')
+insert into Failed (fail_date) values ('2019-01-04')
+insert into Failed (fail_date) values ('2019-01-05')
+Truncate table Succeeded
+insert into Succeeded (success_date) values ('2018-12-30')
+insert into Succeeded (success_date) values ('2018-12-31')
+insert into Succeeded (success_date) values ('2019-01-01')
+insert into Succeeded (success_date) values ('2019-01-02')
+insert into Succeeded (success_date) values ('2019-01-03')
+insert into Succeeded (success_date) values ('2019-01-06')
+
+
+# Write your MySQL query statement below
+
+WITH alldate AS (
+    SELECT 
+        fail_date AS date,
+        'failed' AS period_state
+    FROM Failed
+    UNION ALL
+    SELECT
+        success_date AS date,
+        'succeeded' AS period_state
+    FROM Succeeded
+),
+tpos AS (
+    SELECT *,
+        ROW_NUMBER() OVER (ORDER BY date) -
+        RANK() OVER (PARTITION BY period_state ORDER BY date) AS pos
+    FROM alldate
+    WHERE date BETWEEN '2019-01-01' AND '2019-12-31'
+    ORDER BY date
+)
+SELECT 
+    period_state,
+    MIN(date) AS start_date,
+    MAX(date) AS end_date
+FROM tpos
+GROUP BY pos, period_state
+ORDER BY start_date;
