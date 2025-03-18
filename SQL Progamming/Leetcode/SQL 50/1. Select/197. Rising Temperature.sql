d9f8b09258519c3e503c18bb14d4c56539e7dd1e
SathwikHegde <hegde.sat@northeastern.edu>
2025-03-17 19:35:27 -0600

    Leetcode SQL 17th Mar 25

--- SQL Progamming/Leetcode/SQL 50/1. Select/197. Rising Temperature.sql
+++ SQL Progamming/Leetcode/SQL 50/1. Select/197. Rising Temperature.sql
@@ -51,7 +51,6 @@ insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20
 insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
 
 
-
 # Write your MySQL query statement below
 SELECT W1.id
 FROM Weather W1
