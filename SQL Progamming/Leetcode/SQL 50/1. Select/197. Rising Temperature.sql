26865517d664042a5ad008d0a6e073c317468224
SathwikHegde <hegde.sat@northeastern.edu>
2025-03-19 20:46:29 -0600

    Leetcode SQL 19th Mar 25

--- SQL Progamming/Leetcode/SQL 50/1. Select/197. Rising Temperature.sql
+++ SQL Progamming/Leetcode/SQL 50/1. Select/197. Rising Temperature.sql
@@ -41,6 +41,7 @@ Explanation:
 In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
 In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
 
+
 #SQL Schema
 
 Create table If Not Exists Weather (id int, recordDate date, temperature int)
